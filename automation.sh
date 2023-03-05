8b9d50e5a9717a0db5dd9bd2dda981a9389ccfc1
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-05 19:03:12 +0530

    Check and install Apache2 service

--- automation.sh
+++ automation.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
 sudo apt update -y
+
+dpkg -s apache2 &> /dev/null  
+
+    if [ $? -ne 0 ]
+
+        then
+            echo "Apache2 is not installed"  
+            sudo apt-get update
+            sudo apt-get install apache2
+
+        else
+            echo    "Apache2 is installed"
+    fi
