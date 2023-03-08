ff3953cd177a5c6823bcb4a2a7bc7b9f93dac388
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-09 00:05:53 +0530

    Archive of access and error logs and copying the log to the AWS S3 Bucket

--- automation.sh
+++ automation.sh
@@ -34,3 +34,19 @@ sudo systemctl enable apache2
 echo "The Apache2 service was stopped, errored, or inactive. The service has been started."
 fi
 
+timestamp=$(date '+%d%m%Y-%H%M%S')
+
+cd /var/log/apache2
+
+sudo tar -cvf "MohdZubair-httpd-logs-${timestamp}.tar" access.log error.log
+ 
+src_dir="/var/log/apache2"
+dest_dir="/tmp"
+
+sudo mv "${src_dir}"/*.tar "${dest_dir}"
+
+archive_file="/tmp/MohdZubair-httpd-logs-${timestamp}.tar"
+
+s3_bucket="s3://upgrad-mohdzubair"
+
+aws s3 cp $archive_file $s3_bucket
